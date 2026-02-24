6c9ade29bfeee1364e9bcb8f33fd11747a251fb5
Mr. Algorithm <11aravipratapsingh@gmail.com>
2026-02-24 17:25:01 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Navbar } from "@/components/ui/Navbar";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -103,6 +104,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans bg-[#0a0a0a] text-white min-h-screen flex flex-col selection:bg-white/10`}
       >
+        <Analytics/>
         <Navbar />
         <div className="flex-1 flex flex-col">
           {children}
